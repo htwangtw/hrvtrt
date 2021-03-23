e6a28ed8a84a82ecb4b96a30a45d7eb351000442
Hao-Ting Wang <htwangtw@gmail.com>
2021-03-23 22:56:27 +0000

    ADD test data
    * add test data generated from real NKI data sub-A00018030, including:
            * fmriprep output, downsampled and trimmed
            * raw physiology data, trimmed to match fmriprep output
    	* relevant metadata
    * add all test data dir to .gitignore so all data are added manually
    * physiogradient/tests/utils.py details the downsampling procedure

---
+++ physiogradient/tests/utils.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import nibabel as nb
+from nilearn import image
+from nilearn import masking
+from nilearn._utils import check_niimg
+import numpy as np
+
+
+def get_test_data_path():
+    return Path(__file__).parent / "data"
+
+
+def downsample_test_data(prefix):
+    data = get_test_data_path() / "fmriprep" / f"{prefix}_task-rest_acq-645_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii.gz"
+    mask = get_test_data_path() / "fmriprep" / f"{prefix}_task-rest_acq-645_space-MNI152NLin2009cAsym_res-2_desc-brain_mask.nii.gz"
+    mask = check_niimg(str(mask), atleast_4d=True)
+    brain = image.index_img(str(data), slice(0, 30))
+    brain = image.math_img("img1*img2", img1=brain, img2=mask)
+    aff_orig = nb.load(str(data)).affine[:, -1]
+    target_affine = np.column_stack([np.eye(4, 3) * 6, aff_orig])
+    downsample_data = image.resample_img(brain, target_affine=target_affine,
+                                         target_shape=(33, 39, 33))
+    downsample_data.set_data_dtype('int8')
+    nb.save(downsample_data, str(get_test_data_path() / "fmriprep" / f'{prefix}_task-rest_downsample.nii.gz'))
