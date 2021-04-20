c533d7aa3ba149e9761d23fe73de488c502f52f1
Hao-Ting Wang <htwangtw@gmail.com>
2021-04-20 12:51:28 +0100

    CI fix (#8)
    * NF fmriprep visual QC tools
    
    Modified from
    https://github.com/SIMEXP/fmriprep-qc
    
    * FIX separate CI and code quality checks
    
    * RF update dependency and rename files approproately
    
    * TEST precommit linter
    
    * TEST precommit linter
    
    * PL black
    
    * TEST black hook
    
    * TEST linter hook

---
+++ physiogradient/qc/tests/test_motion.py
@@ -0,0 +1,8 @@
+from ..motion import *
+from ...tests.utils import get_test_data_path
+from pathlib import Path
+
+confounds = (
+    Path(get_test_data_path())
+    / "sub-test_ses-BAS_task-rest_desc-confounds_timeseries.tsv"
+)
---
+++ physiogradient/utils.py
@@ -0,0 +1,45 @@
+import warnings
+import re
+
+import pandas as pd
+
+
+def read_tsv(filename, **kargs):
+    """
+    Read tsv file
+
+    Parameters
+    ----------
+    filename: str or Path
+        Path to tsv file
+
+    **kargs:
+        other inputs pass to panda.read_csv
+    """
+    if kargs.get("sep", False):
+        raise Exception("There's not need to provide input for `sep`.")
+
+    df = pd.read_csv(filename, sep="\t", **kargs)
+    return _check_tsv(df)
+
+
+def _check_tsv(df):
+    """check if file is tsv"""
+    if df.empty is True:
+        raise ValueError("File is empty or not a tab separated file.")
+    elif "," in df.columns[0]:
+        warnings.warn(
+            "File is might not be a tab separated file, please check input"
+        )
+        return df
+    else:
+        return df
+
+
+def parse_bids_subject(filename):
+    """Get subject and session information form a BIDS filename"""
+    matching = re.match("sub-([A0-9]*)_ses-([A-Z]*)", filename)
+    if matching:
+        return matching.group(1), matching.group(2)
+    else:
+        raise ValueError("Invalid file name. Is the input in BIDS?")
