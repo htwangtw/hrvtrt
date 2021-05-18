f5464690c8148aa487412bb40c2bb1598fb0d535
Hao-Ting Wang <htwangtw@gmail.com>
2021-05-18 10:12:36 +0100

    FIXfigure path

--- bin/qc.py
+++ bin/qc.py
@@ -2,7 +2,7 @@ from hrvtrt.qc.motion import data_qc, plot_qc
 
 bids = "/research/cisc1/projects/critchley_nkiphysio/rawdata"
 fmriprep = "/research/cisc2/projects/critchley_nkiphysio/derivatives/fmriprep"
-output_base = "/research/cisc2/projects/critchley_nkiphysio/hrvtrt/results"
+output_base = "/research/cisc2/projects/critchley_nkiphysio/hrvtrt"
 df = data_qc(bids_path=bids, fmriprep_path=fmriprep)
 
 df.to_csv(
